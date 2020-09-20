d5fba54b6adedcc4af0d8db34298a18af8cf05cd
shivaji <official.shivaji007@gmail.com>
2020-09-20 13:37:56 +0530

    Updates

---
+++ precache-manifest.73d17f512b0a5f243115dcc2d17e6da3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b59ac8b50490b27da8c538bea08fd0d",
+    "url": "/whatsapp-clone2/index.html"
+  },
+  {
+    "revision": "04e9f8611c8a962a4f12",
+    "url": "/whatsapp-clone2/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f0763a9c8002c1da3848",
+    "url": "/whatsapp-clone2/static/css/main.ab6bb230.chunk.css"
+  },
+  {
+    "revision": "04e9f8611c8a962a4f12",
+    "url": "/whatsapp-clone2/static/js/2.21e26179.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone2/static/js/2.21e26179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0763a9c8002c1da3848",
+    "url": "/whatsapp-clone2/static/js/main.c5f3a997.chunk.js"
+  },
+  {
+    "revision": "f4d11118689b443ef164",
+    "url": "/whatsapp-clone2/static/js/runtime-main.dd49bce4.js"
+  },
+  {
+    "revision": "1a4ea1dbebf9d212fdaf5ba24942baa6",
+    "url": "/whatsapp-clone2/static/media/logo.1a4ea1db.svg"
+  }
+]);
